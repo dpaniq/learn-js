e569735e1ba04e81ba2d16c40d3f4bec7edb0a39
dpaniq <14brother@gmail.com>
2019-10-06 17:07:16 +0300

    try

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-alert(2*2);
-alert(2*2);
-asdasd
+let money = +prompt('Ваш бюджет на месяц?', '0'),
+    time  =  prompt('Введите дату в формате YYYY-MM-DD', '2019-10-05');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    oExpenses: {},
+    income: [],
+    savings: false
+
+};
+
+appData.expenses[prompt('Обязательная статья расходов в этом месяце')] = prompt('Во сколько обойдется?');
+// appData.expenses[prompt('Обязательная статья расходов в этом месяце')] = prompt('Во сколько обойдется?');
+
+alert(appData.budget / 30);
+
+console.log(appData);
+
