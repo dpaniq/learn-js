efeeda760da6e8c342da8e3c4e863aee95e1e69d
Bogdan Gromov <42778511+dpaniq@users.noreply.github.com>
2020-02-12 17:53:52 +0300

    react1.2.

--- React/react1/app.jsx
+++ React/react1/app.jsx
@@ -1,22 +1,12 @@
-// function Hero(props) {
-
-//     let count = 0
-
-//     function handeClick() {
-//         count += 1
-//         console.log(count)
-//     }
-
-//     return (
-//         <div className = 'container'>
-//             <div className = 'count'>{ count }</div>
-//             <img src = { props.imageUrl } onClick = { handeClick }/>
-//             <h1>{ props.title }</h1>
-//             <p>{ props.subtitle }</p>
-//         </div>
-//     )
-// }
-
+let ImageCounter = function(props) {
+    // console.log(props)
+    return (
+        <div className = 'image-counter'>
+            <div className = 'count'>{ props.count }</div>
+            <img src = { './img/' + props.image} onClick = { props.onCount }/>
+        </div>
+       )
+}
 let Hero = React.createClass({
     getInitialState: function () {
         return {
@@ -24,48 +14,75 @@ let Hero = React.createClass({
         }
     }, 
 
-    handeClick: function() {
-        console.log('////////////////////////////////////////////////////')
-        console.log(this) // All requirements
+    handeClick: function(event) {
+
+        // console.log('////////////////////////////////////////////////////')
+        // console.log(this) // All requirements
         this.setState(
             {count: this.state.count - 1}
         )
     },
 
     render: function(){
+        console.log(this.props)
         return (
             <div className = 'container'>
-                <div className = 'count'>{ this.state.count }</div>
-                <img src = { this.props.imageUrl } onClick = { this.handeClick }/>
-                <h1>{ this.props.title }</h1>
+                <ImageCounter image = { this.props.image }
+                              count    = { this.state.count }
+                              onCount =  { this.handeClick } />
+                <h1> { this.props.title }</h1>
                 <p>{ this.props.subtitle }</p>
             </div>
         )
     }
 })
 
+let App = React.createClass({
+    render : function () {
+        return (
+            <div>
+                { this.props.heroes.map((hero) => {
+                    return <Hero title={ hero.title } subtitle={ hero.subtitle } image={ hero.image } key={ hero.id }/>
+                    })}
+            </div>
+        )
+    }
+})
+
 // ReactDOM.render(React.createElement(Hero, null ), document.getElementById('root'))
+
+// que: There are FOUR instanceof HERO? ------------> YES/NO?
+
+const data = [
+    {   
+        id: 0,
+        title:"React",
+        subtitle:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+        image:"react.png"
+    },
+
+    {
+        id: 1,
+        title:"Angular",
+        subtitle:`Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
+        image:"angular.png"
+    },
+
+    {
+        id: 2,
+        title:"Vue",
+        subtitle:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+        image:"vue.png"
+    },
+
+    {
+        id: 3,
+        title:"Ember",
+        subtitle:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        image:"ember.png"
+    }
+]
+
 ReactDOM.render(
-    <div>
-        <Hero title="React"
-            subtitle="Library for make UI"
-            imageUrl="./img/react.png"
-            />
-
-        <Hero title="Angular"
-            subtitle="Framework angular"
-            imageUrl="./img/angular.png"
-            />
-
-        <Hero title="Ember"
-            subtitle="Library Ember"
-            imageUrl="./img/ember.png"
-            />
-
-        <Hero title="Vue"
-            subtitle="Framework vue"
-            imageUrl="./img/vue.png"
-            />
-    
-    </div>, 
+    <App heroes={data} />, 
     document.getElementById('root'))
